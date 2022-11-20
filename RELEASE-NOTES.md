8c7ed54ae039943d47bbe353adda8a670a001695
Jan Drenkhahn <jan-dennisd@outlook.at>
2022-11-20 21:26:01 +0100

    init commit

---
+++ RELEASE-NOTES.md
@@ -0,0 +1,18 @@
+# Release Notes
+    
+## v0.1.0-beta2
+
+- Updated manifest to identify system as Unofficial.
+
+## v0.1.0-beta1
+
+- Initial public beta release
+- Basic die rolls
+- Focus/Marksman action support  
+- NPC and Hero sheets
+- Traits
+- Optional settings for: 
+  - Corruption, 
+  - Character Advancement, 
+  - Damage Reduction,
+  - and Item Expenditure/Depletion
---
+++ module/helpers/dice.js
@@ -0,0 +1,77 @@
+export async function RollTest({
+    numberOfDice = 2,
+    numberOfSides = 6,
+    defaultThreshold = 5,
+    focusAction = false,
+    marksmanTrait = false } = {}) {
+
+    let threshold = defaultThreshold;
+    if (focusAction && (focusAction === 'true'))
+    {
+        threshold = threshold - 1;
+    }
+
+    if (marksmanTrait && (marksmanTrait === 'true'))
+    {
+        threshold = threshold - 1;
+    }
+    
+    const rollForumla = `${numberOfDice}d${numberOfSides}cs>=${threshold}`;
+
+    // Execute the roll
+    let result = await new Roll(rollForumla, {}).evaluate({'async': true})
+    let renderedRoll = await renderTemplate("systems/tinyd6/templates/partials/test-result.hbs", { rollResult: result });
+    // let renderedRoll = await result.render({ result: result, template: "systems/tinyd6/templates/partials/test-result.hbs" });
+
+    const chatData = {
+        speaker: ChatMessage.getSpeaker(),
+        content: renderedRoll
+    };
+
+    result.toMessage(chatData);
+}
+
+export function setFocusOption(form, element) {
+    form.find(".die-roller > .roll-dice").each((n, tag) => {
+        tag.dataset.enableFocus = element.checked
+    });
+
+    if (element.checked)
+    {
+        form.find(".action-modifiers .toggle-marksman").prop("disabled", false);
+    }
+    else
+    {
+        const marksmanElement = form.find(".action-modifiers .toggle-marksman");
+        marksmanElement.prop("checked", false);
+        marksmanElement.prop("disabled", true);
+    }
+}
+
+export function setMarksmanOption(form, element)
+{
+    form.find(".die-roller > .roll-dice").each((n, tag) => {
+        tag.dataset.enableMarksman = element.checked;
+    });
+}
+
+export function diceToFaces(value, content)
+{
+    switch (value)
+    {
+        case 1:
+            return "fa-dice-one";
+        case 2:
+            return "fa-dice-two";
+        case 3:
+            return "fa-dice-three";
+        case 4:
+            return "fa-dice-four";
+        case 5:
+            return "fa-dice-five";
+        case 6:
+            return "fa-dice-six";
+    }
+
+    return "fa-dice-d6";
+}
